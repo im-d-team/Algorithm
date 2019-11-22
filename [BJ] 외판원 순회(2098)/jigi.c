856b5e4d0f9a24fdc2b274d4c0c0b7860b225a79
Hoyeonjiki Kim <jigi.kim@gmail.com>
2019-11-22 20:23:24 +0900

    baekjoon: 2098: reduce computation and leave comments

--- [BJ] 외판원 순회(2098)/jigi.c
+++ [BJ] 외판원 순회(2098)/jigi.c
@@ -79,10 +79,11 @@ static inline void list_del(struct list_head *entry)
 	     &pos->member != (head); \
 	     pos = list_prev_entry(pos, member))
 
-struct list_head travel = list_head_init(travel);
-unsigned int **map, **min_cost, nr_city;
-
 struct status {
+	/*
+	 * city: the city where the salesman is currently visiting.
+	 * visited: the bitmap for the cities where the salesman once visited.
+	 */
 	unsigned short city, visited;
 	struct list_head list;
 };
@@ -98,56 +99,81 @@ static inline void status_list_add(struct list_head *list, unsigned short city,
 	list_add_tail(&(status->list), list);
 }
 
+/*
+ * map[a][b]: the distance to `a` from `b`.
+ * cost[a][visited]: the distance to 'a' after visiting all `visited`.
+ */
+unsigned int **map, **cost, nr_city;
+struct list_head travel = list_head_init(travel);
+
 static void map_init(void)
 {
 	unsigned int i, j;
 
 	scanf("%u\n", &nr_city);
 
-	min_cost = malloc(sizeof(unsigned int *) * nr_city);
+	cost = malloc(sizeof(unsigned int *) * nr_city);
 	map = malloc(sizeof(unsigned int *) * nr_city);
 
 	for (i = 0; i < nr_city; i++) {
-		min_cost[i] = calloc(1 << (nr_city-1), sizeof(unsigned int));
+		/*
+		 * Because the salesman starts visiting from a specific city,
+		 * the bit for that city always is 1 on `visited` bitmap. So
+		 * no need to represent one bit for that city.
+		 */
+		cost[i] = calloc(1 << (nr_city-1), sizeof(unsigned int));
 		map[i] = malloc(sizeof(unsigned int) * nr_city);
-
-		scanf("%u", &map[i][0]);
-
-		if (map[i][0]) {
-			min_cost[i][1<<(i-1)] = map[i][0];
-			status_list_add(&travel, i, 1<<(i-1));
-		}
 	}
 
-	for (i = 1; i < nr_city; i++) {
+	for (i = 0; i < nr_city-1; i++) {
 		for (j = 0; j < nr_city; j++)
 			scanf("%u", &map[j][i]);
 	}
+
+	/*
+	 * The salesman starts visiting from the last city. So, the distance to
+	 * `j` from `i`, which is the last city, is identical with the distance
+	 * to `j` after visiting `i`.
+	 */
+	for (j = 0; j < nr_city; j++) {
+		scanf("%u", &map[j][i]);
+
+		if (map[j][i]) {
+			cost[j][1<<j] = map[j][i];
+			status_list_add(&travel, j, 1 << j);
+		}
+	}
+
+	--nr_city;
 }
 
 static unsigned int travel_map(void)
 {
 	struct status *cur;
 
-	unsigned short visited_all = (1 << (nr_city-1)) - 1;
-	unsigned int total = (unsigned int) -1;
+	unsigned short visited_all = (1 << nr_city) - 1;
+	unsigned int total_cost = (unsigned int) -1;
 	unsigned int i;
 
 	while (!list_empty(&travel)) {
 		cur = list_first_entry(&travel, struct status, list);
 
-		for (i = 1; i < nr_city; i++) {
-			unsigned int cost, visited;
+		for (i = 0; i < nr_city; i++) {
+			unsigned int c, v;
 
-			if (((cur->visited >> (i-1)) & 1) || !map[i][cur->city])
+			/*
+			 * Impossible to visit the city where the salesman once
+			 * visited, or has no way to go from current city.
+			 */
+			if (((cur->visited >> i) & 1) || !map[i][cur->city])
 				continue;
 
-			cost = min_cost[cur->city][cur->visited] + map[i][cur->city];
-			visited = cur->visited | (1 << (i-1));
+			c = cost[cur->city][cur->visited] + map[i][cur->city];
+			v = cur->visited | (1 << i);
 
-			if (!min_cost[i][visited] || min_cost[i][visited] > cost) {
-				status_list_add(&travel, i, visited);
-				min_cost[i][visited] = cost;
+			if (!cost[i][v] || cost[i][v] > c) {
+				status_list_add(&travel, i, v);
+				cost[i][v] = c;
 			}
 		}
 
@@ -155,16 +181,21 @@ static unsigned int travel_map(void)
 		free(cur);
 	}
 
-	for (i = 1; i < nr_city; i++) {
-		if (map[0][i] && min_cost[i][visited_all]) {
-			unsigned int cost = map[0][i] + min_cost[i][visited_all];
+	/*
+	 * The salesman visited every city and now it's time to go back to the
+	 * city where the salesman started his/her travel. Find minimum total
+	 * distance of traveling.
+	 */
+	for (i = 0; i < nr_city; i++) {
+		if (map[nr_city][i] && cost[i][visited_all]) {
+			unsigned int c = map[nr_city][i] + cost[i][visited_all];
 
-			if (total > cost)
-				total = cost;
+			if (total_cost > c)
+				total_cost = c;
 		}
 	}
 
-	return total;
+	return total_cost;
 }
 
 int main(void)
@@ -172,5 +203,9 @@ int main(void)
 	map_init();
 	printf("%u\n", travel_map());
 
+	/*
+	 * We have some allocated memories to be freed left, but hey, this is
+	 * the end of this program. Skip freeing those allocated memory.
+	 */
 	return 0;
 }
